956a30bb155bb9429e64ad935f4f370e31c447fa
torchnn <125907509+torchnn@users.noreply.github.com>
2026-09-13 17:07:01 +0900

    test-bunker: mirror production quiz progression

--- projects/music-quiz/Sources/Core/QuizProgression.swift
+++ projects/music-quiz/Sources/Core/QuizProgression.swift
@@ -3,18 +3,29 @@ import Foundation
 struct QuizProgression {
     static let clipDurations: [TimeInterval] = [1, 2, 4, 7, 11, 16]
 
-    private(set) var attempt = 0
+    private(set) var attemptIndex = 0
+    private(set) var isFinished = false
+
+    var maxAttempts: Int { Self.clipDurations.count }
+
+    var attemptNumber: Int {
+        min(attemptIndex + 1, maxAttempts)
+    }
 
     var currentClipDuration: TimeInterval {
-        Self.clipDurations[min(attempt, Self.clipDurations.count - 1)]
+        Self.clipDurations[min(attemptIndex, maxAttempts - 1)]
     }
 
-    var isFinished: Bool {
-        attempt >= Self.clipDurations.count
-    }
-
-    mutating func advance() {
+    mutating func advanceAfterMiss() {
         guard !isFinished else { return }
-        attempt += 1
+        attemptIndex += 1
+        if attemptIndex >= maxAttempts {
+            isFinished = true
+        }
+    }
+
+    mutating func finishSolved() {
+        guard !isFinished else { return }
+        isFinished = true
     }
 }
